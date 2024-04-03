f9d6a24ad5cddc9f26e23284b0a5889411755b3c
techtorialacademy <122166999+techtorialacademy@users.noreply.github.com>
2024-04-02 19:17:39 -0500

    Update settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': 'Admin12345',
-        'HOST': 'database-1.c3bhgefoqgp7.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'blog-application-db.c3bhgefoqgp7.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'blog-s3-techtorial' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'blog-app-storage' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
